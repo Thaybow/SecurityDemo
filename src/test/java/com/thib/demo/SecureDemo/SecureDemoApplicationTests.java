0bbf2648146aab2a43dfa0bf688fc404ea5dcc51
Tib <thibault.vandamme@minfin.fed.be>
2021-09-18 13:30:22 +0200

    Initial commit

---
+++ src/test/java/com/thib/demo/SecureDemo/SecureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thib.demo.SecureDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
